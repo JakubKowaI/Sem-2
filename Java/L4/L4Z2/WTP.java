7814e03edd9a73a11417ef31e82fcc37cc2b9853
Jakub Kowal <ikubutek@gmail.com>
2024-04-20 21:45:34 +0200

    Kontynuacja L4Z2

--- Java/L4/L4Z2/WTP.java
+++ Java/L4/L4Z2/WTP.java
@@ -1,44 +1,46 @@
-import java.awt.Label;
-import java.awt.TextArea;
-
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
 
 
-public class WTP extends application{
+public class WTP extends Application{
     public static void main(String[] args) {
         launch(args);
     }
+
     @Override
-    public static void start(Stage primaryStage){
-        GridPane grid = new GridPane();
-        grid.setHgap(10);
-        grid.setVgap(10);
-        grid.setPadding(new Insets(0, 10, 0, 10));
+    public void start(Stage primaryStage){
+        
 
         TextArea dane = new TextArea();
         Button przycisk = new Button("Licz");
-        //Label wynik = new Label();
-        grid.add(dane);
-        grid.add(przycisk);
+        Label wynik = new Label();
+        BorderPane root = new BorderPane(wynik,dane,null,przycisk,null);
+        
         przycisk.setOnAction(new EventHandler<ActionEvent>() { 
             @Override
             public void handle(ActionEvent e) {
-            for(int i=0;i<=Integer.parseInt(dane.getText());i++){
+                String s="";
+            for(int i=1;i<=Integer.parseInt(dane.getText());i++){
                 WierszTrojkataPascala w = new WierszTrojkataPascala(i);
-                grid.add(new Label(w));
-            }            
+                s+=w.wypisz()+ "\n";
+            }
+            wynik.setText(s);
         dane.setText("");
             }
             });
-
-        primaryStage.setScene(new Scene(grid, 300, 250));
+            wynik.setAlignment(Pos.CENTER);
+            Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("WTP");
         primaryStage.show();
     }
 }
